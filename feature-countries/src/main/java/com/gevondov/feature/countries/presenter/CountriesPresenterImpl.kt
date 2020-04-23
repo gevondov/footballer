0bb6a5cb26a259a0d4ed5087988ca07c1053b612
Gurgen Gevondov <g.gevondov@gmail.com>
2020-04-24 00:48:27 +0400

    Init initial state

--- feature-countries/src/main/java/com/gevondov/feature/countries/presenter/CountriesPresenterImpl.kt
+++ feature-countries/src/main/java/com/gevondov/feature/countries/presenter/CountriesPresenterImpl.kt
@@ -1,24 +1,15 @@
 package com.gevondov.feature.countries.presenter
 
-import com.gevondov.common.models.feature.Country
 import com.gevondov.feature.countries.store.CountriesStateStore
 
 class CountriesPresenterImpl(
     private val stateStore: CountriesStateStore
 ) : CountriesPresenter {
 
-    fun init() {
+    init {
         stateStore.applyState(
-            toolbarTitle = "Countries",
-            countries = (1..10).map { Country("Country $it") }
+            toolbarTitle = "Countries"
         )
     }
-
-    fun onCountries(countries: List<Country>) {
-        stateStore.applyState(
-            countries = countries
-        )
-    }
-
 
 }
