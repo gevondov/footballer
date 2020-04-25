ec887027e723dc6ba3f409bec8e5bd5fc5df62f0
Gurgen Gevondov <g.gevondov@gmail.com>
2020-04-25 16:28:50 +0400

    Change countries count

--- feature-countries/src/main/java/com/gevondov/feature/countries/model/CountriesModelImpl.kt
+++ feature-countries/src/main/java/com/gevondov/feature/countries/model/CountriesModelImpl.kt
@@ -5,6 +5,6 @@ import com.gevondov.common.models.feature.Country
 // TODO Move implementation to separate module
 class CountriesModelImpl : CountriesModel {
 
-    override fun loadCountries() = (1..10).map { Country("Country $it") }
+    override fun loadCountries() = (1..50).map { Country("Country $it") }
 
 }
